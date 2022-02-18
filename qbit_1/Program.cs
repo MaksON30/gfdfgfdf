6d65e97962d55499747310e7c5d5bd4253898dd7
MaksON30 <76399278+MaksON30@users.noreply.github.com>
2022-02-18 20:53:16 +0200

    Добавьте файлы проекта.

---
+++ qbit_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qbit_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine()); //price
+            int P = Int32.Parse(Console.ReadLine());//percent
+            int M = Int32.Parse(Console.ReadLine()); //money
+            double doublePersent;
+            double newMoney;
+            double res;
+           
+            doublePersent = (double)(P / 100.0);
+            newMoney = N * doublePersent;
+            double item = N + newMoney;
+            res = M / item;
+            decimal dRes = Convert.ToDecimal(res);
+            Console.WriteLine(Math.Truncate(dRes));
+           // Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
